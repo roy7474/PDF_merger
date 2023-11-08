266b186aaf57369af1b35a91dcff79ce33f3a565
Roy Contreras Benitez <roy7474@gmail.com>
2023-11-07 19:39:08 -0500

    two if loops have been added to the program

--- pdf_merger.py
+++ pdf_merger.py
@@ -4,3 +4,9 @@ import os
 
 merger = PyPDF2.PdfFileMerger()
 
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        merger = PyPDF2.PdfFileMerger()
+        merger.append(file)
+    merger.write("NewCombinedFile.pdf")
+    # print(file)
